4ab79fbaff48c97c03501f0187325f9796109777
XOYZ-Code <XOYZ-Code@users.noreply.github.com>
2020-03-02 23:10:53 +0100

    Elastic bunch Graph Matching
    Argument -interaction nonstopmode im Compiler hinzugefügt
    2 neue Quellen
    1 neues Bild
    Elastc bunch Graph Matching angefangen

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import time
 
 start_time = time.time()
 print('Running LuaLaTeX')
-os.system('lualatex main.tex')
+os.system('lualatex -interaction nonstopmode main.tex')
 print('Running LuaLaTeX a second time')
-os.system('lualatex main.tex')
+os.system('lualatex -interaction nonstopmode main.tex')
 t = time.time() - start_time
 print('Building finished in ' + str(round(t, 2)) + 's')
